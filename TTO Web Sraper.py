0aaa2e450c0736e17dde4d154a5b92aacb64d333
ChunkyMrC <rwalkden@protonmail.com>
2025-02-25 12:12:51 +0000

    fourth

--- TTO Web Sraper.py
+++ TTO Web Sraper.py
@@ -1,13 +1,52 @@
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+import re
 
 ###################### TTO USERNAME & PASSWORD ####################
 
-username = "your email"
-password = "your password"
+while True:
+    username = input("Enter your the email address you use for TTO: ")
+    reg = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if re.match(reg, username):
+        print("Valid email.")
+        break
+    else:
+        print("Invalid email.")
 
-########################### TARGET DATE ###########################
+while True:
+    password = input("Enter your TTO password: ")
+    if isinstance(password, str) and len(password) >= 4:
+        print("Valid password.")
+        break
+    else:
+        print("Invalid password.")
 
-start_dt = date(2025, 1, 25)
-end_dt = date(2025, 2, 23)
+######################### Start and End Dates #########################
+
+while True:
+    start_dt = input("Enter the start date (DD/MM/YY): ")
+    try:
+        input_date = datetime.strptime(start_dt, "%d/%m/%y")
+        print("Start date valid")
+        start_dt = input_date.date()
+        break
+    except ValueError:
+        print("Start date invalid")
+
+while True:
+    end_dt = input("Enter the end date (DD/MM/YY): ")
+    try:
+        input_date = datetime.strptime(end_dt, "%d/%m/%y")
+        print("End date valid")
+        end_dt = input_date.date()
+        break
+    except ValueError:
+        print("End date invalid")
+
 delta = timedelta(days=1)
+dates_to_search = set()
+dates_to_search.add(end_dt)
+
+while start_dt <= end_dt:
+    dates_to_search.add(start_dt)
+    start_dt += delta
 
