38a2240687663291175c5b9d9e2fd5e58809d446
ChunkyMrC <rwalkden@protonmail.com>
2025-02-25 10:40:56 +0000

    third commit

--- TTO Web Sraper.py
+++ TTO Web Sraper.py
@@ -1,6 +1,13 @@
-###################### TTO USERNAME & PASSWORD #######################
+from datetime import date, timedelta
 
-username =
-password =
+###################### TTO USERNAME & PASSWORD ####################
+
+username = "your email"
+password = "your password"
 
 ########################### TARGET DATE ###########################
+
+start_dt = date(2025, 1, 25)
+end_dt = date(2025, 2, 23)
+delta = timedelta(days=1)
+
